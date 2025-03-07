ef8dd2820cd6d4afb902bbab241ed58809747448
BassaniDavid <davbassan98@gmail.com>
2025-03-07 14:19:05 +0100

    feat: snack1 add race bike array whit objects

---
+++ snack1/app.js
@@ -0,0 +1,20 @@
+
+// array di oggetti
+const bikeRaceArray = [
+    // oggetti bici
+    {
+        nome: 'VAN RYSEL NCR CF SHIMANO 105 12V grigia',
+        peso: 8.75,
+    },
+    {
+        nome: ' TRIBAN RC 520 Microshift 10V',
+        peso: 10.4,
+    },
+    {
+        nome: 'Cannondale Supersix - 56',
+        peso: 8.7,
+    },
+
+]
+
+console.log(bikeRaceArray)
