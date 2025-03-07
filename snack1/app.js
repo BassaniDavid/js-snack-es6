9f16b080ced8298480363bbd32924bd3e396b0b0
BassaniDavid <davbassan98@gmail.com>
2025-03-07 14:38:52 +0100

    feat: snack1 complete

--- snack1/app.js
+++ snack1/app.js
@@ -18,3 +18,23 @@ const bikeRaceArray = [
 ]
 
 console.log(bikeRaceArray)
+
+let lighter = 100;//variabile per confronto peso
+
+let bikeLighter;//variabile per assegnazione dei dati della bici più leggera
+
+// confronto il peso delle bici
+for(let i = 0; i<bikeRaceArray.length; i++){
+    if(bikeRaceArray[i].peso<lighter){
+        lighter = bikeRaceArray[i].peso
+        bikeLighter = bikeRaceArray[i]
+    }
+}
+
+console.log(lighter)
+console.log(bikeLighter)
+
+// HTML elements
+const pElement = document.querySelector('p')
+
+pElement.innerHTML = `${bikeLighter.nome}, con un peso di ${bikeLighter.peso}`
