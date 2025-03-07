7ecb8a8be696138986324441166306c40f95b294
BassaniDavid <davbassan98@gmail.com>
2025-03-07 14:47:16 +0100

    feat: add array with objects

---
+++ snack2/app.js
@@ -0,0 +1,19 @@
+
+// array contenente oggetti squadre
+const soccerLeague = [
+    {
+        name: 'Sassuolo',
+        score: 0,
+        foulPlaySuffer: 0,
+    },
+    {
+        name: 'Cagliari',
+        score: 0,
+        foulPlaySuffer: 0,
+    },
+    {
+        name: 'Lecce',
+        score: 0,
+        foulPlaySuffer: 0,
+    },
+]
